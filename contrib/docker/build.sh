4bf8753d0ff4d92465064fa783793996b3dffea4
Martin Weinelt <hexa@darmstadt.ccc.de>
2017-12-19 22:50:31 +0100

    add support for gpg-signed source tarballs

--- contrib/docker/build.sh
+++ contrib/docker/build.sh
@@ -5,6 +5,13 @@ set -e
 sudo pacman -Sy
 
 cd /pkg
+
+# import gpg keys if necessary
+(source PKGBUILD;
+ if [ ! -z $validpgpkeys ]; then
+        gpg --recv-keys $validpgpkeys
+ fi)
+
 makepkg -fs --noconfirm MAKEFLAGS=-j$JOBS
 
 exit 0
