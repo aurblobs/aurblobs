a29cfe9a95e152547c53ae4a5ae1d5bc2e7524eb
Martin Weinelt <hexa@darmstadt.ccc.de>
2017-12-19 22:52:46 +0100

    sign packages in sign.sh entry point

--- contrib/docker/sign.sh
+++ contrib/docker/sign.sh
@@ -4,8 +4,18 @@ set -e
 
 gpg --import /privkey.gpg
 
+cd /pkg
+
+PKGS=$(basename -a `ls /pkg/*.pkg.tar.xz` | tr '\n' ' ')
+
+# sign packages
+for PKG in $PKGS; do
+    gpg --detach-sign --no-armor $PKG
+done
+
 cd /repo
 cp /pkg/*.pkg.tar.* .
-repo-add --sign --delta --remove $REPO_NAME.db.tar.gz $(basename -a `ls /pkg/*.pkg.tar.xz` | tr '\n' ' ')
+
+repo-add --sign --delta --remove $REPO_NAME.db.tar.gz $PKGS
 
 exit 0
